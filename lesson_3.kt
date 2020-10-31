96b3bf8b2f24ea1d4655a40782fddf8739e67adb
kamioka24 <keee2404@gmail.com>
2020-10-31 23:53:24 +0900

    [配列]

--- lesson_3.kt
+++ lesson_3.kt
@@ -1,3 +1,14 @@
+// 配列 ---------------------------------------------
+
+val rnd = Random
+val number = rnd.nextInt(20) // 0 ~ 19
+val omikujiShelf = Array<String>(20, {"吉"})
+omikujiShelf[0] = "大吉"
+omikujiShelf[19] = "凶"
+val str = omikujiShelf[number]
+// 大吉、凶が出る確率は低い
+
+
 // 配列を使ったforループ
 val array = arrayOf("a", "b", "c", "d", "e")
 for (i in array) println(i)
