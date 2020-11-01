f0e6d27592598cddaba48f2336ab936b6689b299
kamioka24 <keee2404@gmail.com>
2020-11-01 21:50:55 +0900

    [配列]

--- lesson_3.kt
+++ lesson_3.kt
@@ -1,14 +1,21 @@
-// 配列 ---------------------------------------------
+// 配列 ---------------------------------------------------
 
 val rnd = Random
 val number = rnd.nextInt(20) // 0 ~ 19
-val omikujiShelf = Array<String>(20, {"吉"})
+val omikujiShelf = Array<String>(20, {"吉"}) // Arrayクラスの宣言と初期化
+// Array<配列の型>(要素の数, {初期化処理})
 omikujiShelf[0] = "大吉"
 omikujiShelf[19] = "凶"
 val str = omikujiShelf[number]
 // 大吉、凶が出る確率は低い
 
 
-// 配列を使ったforループ
+// 配列を使った【forループ】----------------------------------
+
+/* for( 任意の変数名 in イテレータブルなオブジェクト ) {
+    処理
+   }
+*/
+// イテレータブルなオブジェクト => 配列のように順番にアクセスできるもの
 val array = arrayOf("a", "b", "c", "d", "e")
-for (i in array) println(i)
+for(i in array) println(i)
