93c2761b212fbec669cdb8c98bc3eadf17da37d0
kamioka24 <keee2404@gmail.com>
2021-01-14 15:46:11 +0900

    [Add]

---
+++ lesson_5.kt
@@ -0,0 +1,16 @@
+// よく使う関数
+let / apply / also / ran / map
+
+
+A ?: B // nullを潰す
+// AがnullだったらBになる
+val A = null
+println A ?: B
+// =>  B
+
+
+hoge?.let(it == boke)
+// hogeがnullだったらbokeになる
+val hoge = null
+println hoge?.let(it == boke)
+// => boke
