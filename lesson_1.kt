53be7c87deda6bfb4415b75816753d3a9fb4b7d2
kamioka24 <keee2404@gmail.com>
2021-01-17 18:01:29 +0900

    [言語仕様]

--- lesson_1.kt
+++ lesson_1.kt
@@ -1,18 +1,19 @@
-// 基本構文
+// 最初に押さえておきたいKotlinの言語仕様
+
+// 【基本構文】
 fun main() {
     println("Hello World")
 }
-
-/*
-複数行コメント
-複数行コメント
-複数行コメント
- */
+print    ->  改行なし
+println  ->  改行あり
 
 
-// 変数
-// vel => 定数 (再代入できない)
-// ver => 変数 (再代入できる)
+// 【定数と変数】
+vel -> 定数 (再代入できない)
+constをつけることで定義
+例) const val TAX_NUMBER = 1.10 // アンダースコア区切りの大文字
+
+ver -> 変数 (再代入できる)
 fun main(args: Array<String>) {
     // val msg: String = "Hello World"
     val msg = "Hello World" // 型推論(上の行の省略形)
@@ -20,20 +21,54 @@ fun main(args: Array<String>) {
 }
 
 
-// データ型
-/*
+// 【データ型】
+
 <<文字列>> String, Char
 Stringは " " で囲む          val s = "Hello"
 Charは ' ' で囲む            val c = 'World'
 
-<<整数値>> Byte, Short, Int, Long (右に行くほど大きい桁数を扱える。通常はInt)
+<<整数値>> Byte, Short, Int, Long // 右に行くほど大きい桁数を扱える。通常はInt
 val i: Int = 100
-val l: Long = 55555555L (数値の最後に大文字のLをつける)
+val l: Long = 55555555L // 数値の最後に大文字のLをつける
 
-<<浮動点少数>> Float, Double (Doubleの方が桁数が大きいものを扱える)
-val f: Float = 123.456F (数値の最後に大文字か小文字のFをつける)
+<<浮動点少数>> Float, Double // Doubleの方が桁数が大きいものを扱える
+val f: Float = 123.456F // 数値の最後に大文字か小文字のFをつける
 val d: Double = 123.456
 
 Boolean (true / false)
 val flag: Boolean = true
-*/
+
+
+// 【null typeとnone null type】
+// できるだけnone null typeを使う
+var a: String = "abc"  // none null type
+a = null               // 代入できない。エラーが出る
+val l = a.length       // 文字の長さを出力
+
+var a: String? = "abc" // null type
+a = null               // 代入できる
+val l = a.length       // nullチェックしていないためエラーが出る。(a!!.lengthにすればできる)
+
+
+// 【nullなら実行しない安全呼び出し】
+val thread: Thread? = null
+thread?.start() // -> 何も起こらない
+
+
+// 【nullの代替値】
+textView.setText(name?: "no name")
+
+// dataがnullになるならreturnする
+val data = Hoge.getData ?: return
+
+
+// 【raw string】
+// 改行を含め、書いた通りに表現するにはダブルクォーテーション3つで囲む
+fun main(args: Array<String>) {
+    val text = """
+        |むかしむかし
+        |あるところに
+        |じーさんばーさんが
+        """.trimMargin() // 行頭の | は目印でそれを含まないようにtrimMarginしている
+    println("$text")
+}
