8361a8b0f0542657cecf316410285da8e4be0c90
kamioka24 <keee2404@gmail.com>
2021-03-04 11:14:42 +0900

    [Add]

--- update_1.kt
+++ update_1.kt
@@ -1,6 +1,31 @@
 // 新しいバージョンがある際に、ユーザーにバージョンアップを促すダイアログを出す
 
-// 【方法1】
+// 【方法1】----------------------------------------------------------------------------------------
+
+// graidle へ ライブラリーの記述
+implementation 'com.google.android.play:core:1.9.1'
+
+
+// ダイアログを出したいActivityへ記述
+private val versionCode = BuildConfig.VERSION_CODE // バージョンコードの取得
+
+val appUpdateManager = AppUpdateManagerFactory.create(this) // マネージャーのインスタンスを作成
+
+val appUpdateInfoTask = appUpdateManager.appUpdateInfo // 更新の確認に使用するインテントオブジェクトを返す
+
+// バージョンが古ければアップデートを促すダイアログを出す。
+appUpdateInfoTask.addOnSuccessListener { appUpdateInfo -> // 指定された更新の許可を確認
+    if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+        && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+        appUpdateManager.startUpdateFlowForResult(
+            appUpdateInfo,
+            AppUpdateType.IMMEDIATE,
+            this,
+            versionCode)
+    }
+}
+
+// 不要かも
 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
     val channelId = getString(R.string.default_notification_channel_id)
     val channelName = getString(R.string.default_notification_channel_name)
@@ -12,7 +37,7 @@ if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
 
 
-// 【方法2】
+// 【方法2】----------------------------------------------------------------------------------------
 if (versionCode <= 13) {
     AlertDialog.Builder(this)
         .setCancelable(false) // OKボタンを押さないとダイアログが消えない設定
